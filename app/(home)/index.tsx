c5bd4af6ebba8c67bb38c78ae2aa38c31e1989fb
sheninthjr <sheninthjr23@gmail.com>
2024-05-21 00:47:03 +0530

    Dashboard is initialized

---
+++ app/(home)/index.tsx
@@ -0,0 +1,18 @@
+import { ScrollView, Text, View } from "react-native";
+
+export default function Home() {
+  return (
+    <>
+      <ScrollView>
+        <View>
+          <Text>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi porro
+            nesciunt iste, atque molestias provident omnis quae facere cum
+            repellendus debitis perferendis non pariatur optio unde aperiam
+            sapiente, reiciendis iusto!
+          </Text>
+        </View>
+      </ScrollView>
+    </>
+  );
+}
